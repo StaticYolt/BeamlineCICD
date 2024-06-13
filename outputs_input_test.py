0ad24a67fe3aa7834fe5a94a2fa833bf38234cde
StaticYolt <126204653+StaticYolt@users.noreply.github.com>
2024-06-13 11:44:52 -0400

    added second test case supposed to not work

--- outputs_input_test.py
+++ outputs_input_test.py
@@ -3,4 +3,7 @@ import pytest
 from outputs_input import output_input
 
 def test_func_one():
-    assert "Hello" ==output_input("Hello")
+    assert "Hello" == output_input("Hello")
+
+def test_func_two_error():
+    assert "ERROR" == output_input(":P")
