9c41a3aed86a8ba805c7e04f7dae255b2ac2b672
StaticYolt <126204653+StaticYolt@users.noreply.github.com>
2024-06-13 11:42:53 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BeamlineCICD',
+    version='3.11',
+    packages=[''],
+    url='',
+    license='',
+    author='Onepiece4',
+    author_email='',
+    description=''
+)
